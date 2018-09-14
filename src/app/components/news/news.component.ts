f9def714638a63271f395d7771fa5cbd7bb854da
Beatons <studentas.nerijus@gmail.com>
2018-09-14 22:05:06 +0200

    Pagination, Newsfeed table, Sources, and search.

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -11,20 +11,46 @@ import { Observable } from "rxjs";
 
 export class GGNewsComponent {
   
-  sources: Observable<NewsAPI.SourceResult>
-
+  sources: Observable<NewsAPI.SourceResult>;
+  feed: Observable<NewsAPI.NewsResult>;
+  searchInput = '';
+  sourcesInput: string[] = [];
+  currentPage = 1;
+  pageSize = 5;
   constructor(
     private newsApi:NewsApiService,
-    private feedStore: FeedStoreService
+    // private feedStore: FeedStoreService
     ) {}
     ngOnInit():void {
       this.getSources();
     }
 
+    getNews():void {
+      console.log(this.sourcesInput);
+      if(this.sourcesInput.length > 0) {
+        this.feed = this.newsApi.getNews({
+          page:this.currentPage,
+          pageSize: this.pageSize,
+          sortBy: "popularity",
+          q:this.searchInput,
+          sources:this.sourcesInput
+        });
+      }
+    }
     private getSources():void {
       this.sources = this.newsApi.getSources();
     }
     saveAsNew() {
-      
+
+    }
+    changePage(direction:string) {
+      if(direction === 'back') {
+        this.currentPage--;
+        this.getNews();
+      }
+      else {
+        this.currentPage++;
+        this.getNews();
+      }
     }
 }
